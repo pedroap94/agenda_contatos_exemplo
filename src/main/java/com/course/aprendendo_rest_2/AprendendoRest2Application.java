a2d06f06e08eb4e1750a3f7df126826de9b38edb
Pedro Almodovar Pinto <pedroalmodovar94@gmail.com>
2021-02-08 11:17:57 -0300

    Criacao do Projeto

---
+++ src/main/java/com/course/aprendendo_rest_2/AprendendoRest2Application.java
@@ -0,0 +1,13 @@
+package com.course.aprendendo_rest_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoRest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoRest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/aprendendo_rest_2/AprendendoRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.aprendendo_rest_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoRest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
